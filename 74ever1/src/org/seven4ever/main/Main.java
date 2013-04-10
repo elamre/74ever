3330a8a086c7aea71f7ee57da49280fcf74bde4b
marius <marius@ubuntu.ubuntu-domain>
2013-04-10 15:04:29 +0200

    test

---
+++ 74ever1/src/org/seven4ever/main/Main.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.seven4ever.main;
+
+
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("st");
+    }
+}
