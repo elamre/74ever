ec405fe088c56738dd2bed162b72e9dc71e24b80
Elmar <elmarblabla@hotmail.com>
2013-04-10 15:47:29 +0200

    Added TouchWrapper

--- 74ever/src/org/seven4ever/main/Main.java
+++ 74ever/src/org/seven4ever/main/Main.java
@@ -4,11 +4,7 @@
  */
 package org.seven4ever.main;
 
-/**
- *
- * @author merijn
- */
-import lejos.nxt.Motor;
+
 public class Main {
 
     /**
@@ -16,6 +12,6 @@ public class Main {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("test");
+        System.out.println("teest");
     }
 }
---
+++ 74ever/src/org/seven4ever/sensor/SensorManager.java
@@ -0,0 +1,11 @@
+package org.seven4ever.sensor;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Elmar
+ * Date: 4/10/13
+ * Time: 3:28 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class SensorManager {
+}
