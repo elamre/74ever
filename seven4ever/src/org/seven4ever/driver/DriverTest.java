309850e5bb6b2b06ab55a360ed672b8578d8f1b6
Max Schachtschabel <max.schachti@web.de>
2013-04-12 14:24:35 +0200

    Added logging

--- seven4ever/src/org/seven4ever/driver/DriverTest.java
+++ seven4ever/src/org/seven4ever/driver/DriverTest.java
@@ -1,23 +1,25 @@
 package org.seven4ever.driver;
 
+import org.seven4ever.util.Logger;
+
 /**
- * Created with IntelliJ IDEA.
- * User: Elmar
- * Date: 4/12/13
- * Time: 2:06 PM
- * To change this template use File | Settings | File Templates.
+ * Created with IntelliJ IDEA. User: Elmar Date: 4/12/13 Time: 2:06 PM To change
+ * this template use File | Settings | File Templates.
  */
 public class DriverTest {
-    public static void main(String[] args) throws Exception{
-        Driver driver;
-        Thread thread;
-        driver = new Driver();
-        thread = new Thread(driver);
-        thread.start();
-        driver.setSpeed(1700);
-        Thread.sleep(2000);
-        driver.setSpeed(100);
-        driver.setRotation(10);
-        Thread.sleep(1000);
-    }
+	public static void main(String[] args) throws Exception {
+		Driver driver;
+		Thread thread;
+		driver = new Driver();
+		thread = new Thread(driver);
+		thread.start();
+		Logger.getInstance().debug("1");
+		driver.setSpeed(1700);
+		Thread.sleep(2000);
+		Logger.getInstance().debug("2");
+		driver.setSpeed(100);
+		driver.setRotation(40);
+		Thread.sleep(1000);
+		Logger.getInstance().debug("3");
+	}
 }
