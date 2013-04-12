178ed882ddf981a47048bbfdb0ebda8ffc7f561a
Elmar <elmarblabla@hotmail.com>
2013-04-12 13:18:33 +0200

    Mapped the ports to the right position

--- seven4ever/src/org/seven4ever/util/Ports.java
+++ seven4ever/src/org/seven4ever/util/Ports.java
@@ -13,17 +13,17 @@ import lejos.nxt.SensorPort;
  */
 public class Ports {
     /** The touch sensor at the front of the robot for front collisions */
-    public static final SensorPort TOUCHPORTFRONT = SensorPort.S1;
+    public static final SensorPort TOUCHPORTFRONT = SensorPort.S3;
     /** The touch sensor at the back of the robot */
-    public static final SensorPort TOUCHPORTBACK = SensorPort.S2;
+    public static final SensorPort TOUCHPORTBACK = SensorPort.S4;
     /** The vision sensor to detect obstacles and walls */
-    public static final SensorPort VISIONPORT = SensorPort.S3;
+    public static final SensorPort VISIONPORT = SensorPort.S1;
     /** The light sensor to detect restricted zones */
-    public static final SensorPort LIGHTPORT = SensorPort.S4;
+    public static final SensorPort LIGHTPORT = SensorPort.S2;
     /** The vision motor port to rotate the vision sensor */
-    public static final NXTRegulatedMotor VISIONMOTORPORT = Motor.A;
+    public static final NXTRegulatedMotor VISIONMOTORPORT = Motor.C;
     /** The joint motor port to rotate the robot */
     public static final NXTRegulatedMotor JOINTMOTORPORT = Motor.B;
     /** The motor port to move the robot */
-    public static final NXTRegulatedMotor MOTORPORT = Motor.C;
+    public static final NXTRegulatedMotor MOTORPORT = Motor.A;
 }
