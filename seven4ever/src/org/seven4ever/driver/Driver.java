dff67134bf576b05b55bdd8a93874884bb169947
merijn <merijn@mintbox>
2013-04-16 16:47:15 +0300

    added rotation function

--- seven4ever/src/org/seven4ever/driver/Driver.java
+++ seven4ever/src/org/seven4ever/driver/Driver.java
@@ -7,14 +7,20 @@ package org.seven4ever.driver;
 import org.seven4ever.util.Logger;
 import org.seven4ever.util.Ports;
 
-/** @author merijn */
+/**
+ * @author merijn
+ */
 public class Driver implements Runnable {
+
     private DriverState state = DriverState.IDLE;
-    /** Left = up, right = down. True-False */
+    /**
+     * Left = up, right = down. True-False
+     */
     private int targetRotation = 0;
     private int motorTimer = 100;
+    private int currentDegree = 0;
+    private int turnDegree;
     int neutral = 10; // set the neutral value of the motor
-
 
     public Driver() {
         neutral = Ports.JOINTMOTORPORT.getPosition();
@@ -26,7 +32,7 @@ public class Driver implements Runnable {
      * @param speed
      */
     public void setSpeed(Speed speed) {
-    	Ports.MOTORPORT.setSpeed(speed.getSpeed());        
+        Ports.MOTORPORT.setSpeed(speed.getSpeed());
     }
 
     /**
@@ -34,35 +40,49 @@ public class Driver implements Runnable {
      *
      * @param degrees
      */
-    public void setRotation(int degrees) {
-        Ports.JOINTMOTORPORT.rotate(degrees);
+    public void setRotation(int degree) {
+        turnDegree = degree - currentDegree;
+        if (Math.abs(turnDegree) < 170) {
+            Ports.JOINTMOTORPORT.rotate(turnDegree);
+            Logger.getInstance().debug("rotating to: " + degree);
+        } else if (Math.abs(turnDegree) >= 170){
+            Ports.JOINTMOTORPORT.rotate(turnDegree);
+            Logger.getInstance().error("Turn too large!");
+        }
+        currentDegree = degree;
     }
 
-    /** The slow stop */
+    /**
+     * The slow stop
+     */
     public void stop() {
         state = DriverState.STOPPING;
     }
 
-    /** Emergency stop, stops the car immediately */
+    /**
+     * Emergency stop, stops the car immediately
+     */
     public void emergencyStop() {
         state = DriverState.EMERGENCYSTOP;
     }
 
-    public void moveForward(Speed speed){
-    	setSpeed(speed);
-    	state = DriverState.MOVING;
-    	Ports.MOTORPORT.forward();
-    	motorTimer = 100;
-    }
-    
-    public void moveBackward(Speed speed){
-    	setSpeed(speed);
-    	state = DriverState.MOVING;
-    	Ports.MOTORPORT.backward();
-    	motorTimer = 100;
+    public void moveForward(Speed speed) {
+        setSpeed(speed);
+        state = DriverState.MOVING;
+        Ports.MOTORPORT.forward();
+        motorTimer = 100;
     }
 
-    /** @return  */
+    public void moveBackward(Speed speed) {
+        setSpeed(speed);
+        state = DriverState.MOVING;
+        Ports.MOTORPORT.backward();
+        motorTimer = 100;
+    }
+
+    /**
+     * @return
+     */
     public DriverState getState() {
         return state;
     }
@@ -75,7 +95,7 @@ public class Driver implements Runnable {
                     if (Ports.MOTORPORT.isMoving()) {
                         Ports.MOTORPORT.stop();
                     }
-                                                // Do nothing
+                    // Do nothing
                     break;
                 case STOPPING:
                     int speed = 0;
@@ -83,33 +103,33 @@ public class Driver implements Runnable {
                         Ports.MOTORPORT.setSpeed(speed - 5);
                     }
                     if (speed <= 0) {
-                    	speed = 0;
-                    	Ports.MOTORPORT.setSpeed(0);
+                        speed = 0;
+                        Ports.MOTORPORT.setSpeed(0);
                         state = DriverState.IDLE;
                     }
-                                                // Slow down and go to idle, Make sure robot is in neutral position though
+                    // Slow down and go to idle, Make sure robot is in neutral position though
                     break;
                 case MOVING:
-                	motorTimer-=5;
-                	if(motorTimer<=0){
-                		state = DriverState.STOPPING;
-                	}
-                                                // Move as long as the timer > 0 then stop
+                    motorTimer -= 5;
+                    if (motorTimer <= 0) {
+                        state = DriverState.STOPPING;
+                    }
+                    // Move as long as the timer > 0 then stop
                     break;
                 case EMERGENCYSTOP:
-                	Ports.MOTORPORT.stop();
-                	Ports.MOTORPORT.setSpeed(0);
-                                                // Stop right away and stay in this state
+                    Ports.MOTORPORT.stop();
+                    Ports.MOTORPORT.setSpeed(0);
+                    // Stop right away and stay in this state
                     break;
                 case TURNING:
-                                                // Turn, then go to STOPPING state
+                    // Turn, then go to STOPPING state
                     break;
             }
             try {
-				Thread.sleep(5);
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
+                Thread.sleep(5);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
     }
 }
