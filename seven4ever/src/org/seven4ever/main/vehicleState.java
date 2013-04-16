884f50f8213dc5adebc381ae9acc1d3ba3caebf9
merijn <merijn@mintbox>
2013-04-16 10:46:44 +0300

    added enumclass

---
+++ seven4ever/src/org/seven4ever/main/vehicleState.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.seven4ever.main;
+
+/**
+ *
+ * @author merijn
+ */
+public enum vehicleState {
+    IDLESTATE, MOVINGSTATE, TURNINGSTATE, CHECKINGSENSORSTATE, SETTLINGSTATE, SAFEMODE
+}
